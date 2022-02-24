f9a81d6ccba5da5cb1615d66fb224edbc8d64a6c
Gabriel <wootengabriel90@gmail.com>
2022-02-23 22:38:49 -0500

    fixed some errors

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -7,34 +7,34 @@ describe ("caesar", () => {
 
         const actual = caesar(input);
         const expected = false;
-    }
+    })
 
     it("should return false if the shift value is greater than 25", () => {
         const input = "SCRambled Message!";
 
         const actual = caesar(input, 26);
         const expected = false;
-    }
+    })
 
     it("should return false if the shift value is less than -25", () => {
         const input = "SCRambled Message!";
 
         const actual = caesar(input, -26);
         const expected = false;
-    }
+    })
 
     it("should ignore capital letters in the input", () => {
         const input = "SCRambled Message!";
 
         const actual = caesar(input, 4);
         const expected = "wgveqfpih qiwweki!";
-    }
+    })
 
     it("should return should correctly decode an input based on the shift", () => {
         
-    }
+    })
 
     it("should return should correctly encode an input based on the shift", () => {
         
-    }
+    })
 })
