120d745da51960b8fe6f446a3cf9135e7c9a0e1b
Gabriel <wootengabriel90@gmail.com>
2022-02-23 22:23:24 -0500

    progress for half the tests and most of the caesar function; needs calculation for overshift

--- src/caesar.js
+++ src/caesar.js
@@ -6,8 +6,21 @@
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
+  function caesar(input, shift = 0, encode = true) {
+    if (shift < -25 || shift > 25 || shift === 0) return false; /* if given an invalid shift number (greater than 25 or less than 25), or no shift number, the code stops here and returns false */
+    const messageArray = input.toLowerCase().split(""); /* making each letter of the input its own array entry for easier manipulation */
+    const message = messageArray.join(""); /* making the new array into a string for the comparison in the map function below, enables ignoring the capital letters in the input */
+   console.log(message);
+    let letterChange = messageArray.map(function(value, index, array){
+      let newCharCode = message.charCodeAt(index) + shift;
+      if(message.charCodeAt(index) < 97 || message.charCodeAt(index) > 122){
+        return value /* if the value is not a lower case letter it stays the same, keeping spaces and punctuation untouched */
+      }else{
+        return String.fromCharCode(newCharCode) /* adds the shift to the current characters code value and returns a new letter based on the resulting new character code */
+      }
+    });
+    return letterChange.join("");
+
   }
 
   return {
