55ad94a7e0239166bfe7db9afcf9f7e9e6e50e49
Gabriel <wootengabriel90@gmail.com>
2022-02-24 22:34:24 -0500

    completed polybius problem and fixed caesar tests

--- src/polybius.js
+++ src/polybius.js
@@ -6,8 +6,30 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+  /* created two objects for our known encoding and decoding metods*/
+  const encoding = {
+    'a':'11', 'b':'21', 'c':'31', 'd':'41', 'e':'51',
+    'f':'12', 'g':'22', 'h':'32', 'i':'42', 'j':'42', 'k':'52',
+    'l':'13', 'm':'23', 'n':'33', 'o':'43', 'p':'53',
+    'q':'14', 'r':'24', 's':'34', 't':'44', 'u':'54',
+    'v':'15', 'w':'25', 'x':'35', 'y':'45', 'z':'55'
+  }
+  
+  const decoding = {
+    '11':'a', '21':'b', '31':'c', '41':'d', '51':'e',
+    '12':'f', '22':'g', '32':'h', '42':'(i/j)', '52':'k',
+    '13':'l', '23':'m', '33':'n', '43':'o', '53':'p',
+    '14':'q', '24':'r', '34':'s', '44':'t', '54':'u',
+    '15':'v', '25':'w', '35':'x', '45':'y', '55':'z'
+  }
+
   function polybius(input, encode = true) {
-    // your solution code here
+    /* determines which table our code will read  to encode or decode based on the encode variable*/
+    const direction = encode ? encoding : decoding;
+    /* on the next line if our decoding input, without spaces included, is uneven (divisible by 2) then it returns false */
+    if (direction === decoding && input.replace(/\s+/g, '').length % 2) return false;
+    /* our return code matches either 2 numbers or a letter in the lowercased string provided in the input through the map method and returns the corresponding value from the appropriate object or a space (\s) and then joins the resulting array into our new message*/
+    return input.toLowerCase().match(/[0-9]{2}|[a-z]|\s/g).map(character => direction[character] || character).join('');
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,29 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const  { polybius } = require("../src/polybius");
+
+describe ("polybius", () => {
+    it("should return a coded message with spaces maintained and capital letters ignored and should still be a string", () => {
+        const input = "Hello world";
+
+        const actual = polybius(input);
+        const expected = '3251131343 2543241341';
+        expect(actual).to.equal(expected);
+        expect(actual).to.be.a.string;
+    })
+
+    it("should return (i/j) when decoding the number 42 as those letters share a code number", () => {
+        const input = "4432423352125413";
+
+        const actual = polybius(input, false);
+        const expected = "th(i/j)nkful";
+        expect(actual).to.equal(expected);
+    });
+
+    it("should return false if the number of characters in the input when decoding is not even", () => {
+        const input = '3251131343 25432413411';
+
+        const actual = polybius(input, false);
+        const expected = false;
+        expect(actual).to.equal(expected);
+    });
+})
